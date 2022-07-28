52199a22f113c560bd83b23d47615388e52406ef
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-07-28 16:08:42 -0700

    feat: added natspec copy for TransferHook family of contracts (#257)
    * feat: added natspec copy for TransferHook family of contracts
    
    * fix: cr #257 interface copy nits

--- apps/smart-contracts/token/contracts/interfaces/IBlocklistTransferHook.sol
+++ apps/smart-contracts/token/contracts/interfaces/IBlocklistTransferHook.sol
@@ -4,9 +4,18 @@ pragma solidity =0.8.7;
 import "./ITransferHook.sol";
 import "./IAccountList.sol";
 
-//TODO: add all natspecs at the end
+/**
+ * @notice Hook that provides blocklist functionality for token transfers.
+ * A blocked address cannot send or receive the specified ERC20 token.
+ */
 interface IBlocklistTransferHook is ITransferHook {
+  /**
+   * @notice Sets the `IAccountList` contract that specifies the addresses to
+   * block.
+   * @param newBlocklist Address of the `IAccountList` contract
+   */
   function setBlocklist(IAccountList newBlocklist) external;
 
+  ///@return The blocklist contract
   function getBlocklist() external view returns (IAccountList);
 }
--- apps/smart-contracts/token/contracts/interfaces/IRestrictedTransferHook.sol
+++ apps/smart-contracts/token/contracts/interfaces/IRestrictedTransferHook.sol
@@ -4,13 +4,35 @@ pragma solidity =0.8.7;
 import "./IBlocklistTransferHook.sol";
 import "./IAccountList.sol";
 
-//TODO: add all natspecs at the end
+/**
+ * @notice Hook for restricting transfers of an ERC20 token.
+ * @dev Transfers of the specified token are restricted by default.
+ *
+ * Any address can send to an allowlisted destination address.
+ *
+ * Allowlisted source addresses are able to send to any other address
+ * (including addresses not on the destination allowlist).
+ *
+ * Blocklisted addresses cannot send or receive tokens, even if allowlisted.
+ */
 interface IRestrictedTransferHook is IBlocklistTransferHook {
+  /**
+   * @notice Sets the external `IAccountList` contract that specifies the
+   * allowlisted source addresses.
+   * @param newSourceAllowlist Address of the `IAccountList` contract
+   */
   function setSourceAllowlist(IAccountList newSourceAllowlist) external;
 
+  /**
+   * @notice Sets the external `IAccountList` contract that specifies the
+   * allowlisted destination addresses.
+   * @param newDestinationAllowlist Address of the `IAccountList` contract
+   */
   function setDestinationAllowlist(IAccountList newDestinationAllowlist) external;
 
+  ///@return The source allowlist contract
   function getSourceAllowlist() external view returns (IAccountList);
 
+  ///@return The destination allowlist contract
   function getDestinationAllowlist() external view returns (IAccountList);
 }
