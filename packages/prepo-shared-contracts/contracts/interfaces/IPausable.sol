dbd2b46d1b084c62fda2495069a6ca3d8d9ef70c
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-08-12 16:56:51 -0700

    docs: add pausable and protectedhook interface copy (#305)
    * feat: add pausable and protectedhook interface copy
    
    * fix: cr #305 amending nits and removing ProtectedHook natspec
    
    * fix: cr #305 rev2 add todo to remove protectedhook interface

--- packages/prepo-shared-contracts/contracts/interfaces/IPausable.sol
+++ packages/prepo-shared-contracts/contracts/interfaces/IPausable.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity =0.8.7;
 
-// TODO: add natspec comments
+/**
+ * @notice Allows a contract owner to pause their contract.
+ * @dev Pausing a contract will only affect functions using the included
+ * `whenNotPaused` modifier.
+ */
 interface IPausable {
+  /**
+   * @dev Emitted by `setPaused()`.
+   * @param newPaused Whether the contract was paused
+   */
   event PausedChange(bool newPaused);
 
+  /**
+   * @notice Pauses or unpauses the contract.
+   * @dev Only callable by `owner()`.
+   * @param newPaused Whether the contract is to be paused
+   */
   function setPaused(bool newPaused) external;
 
+  /// @return Whether the contract is currently paused
   function isPaused() external view returns (bool);
 }
--- packages/prepo-shared-contracts/contracts/interfaces/IProtectedHook.sol
+++ packages/prepo-shared-contracts/contracts/interfaces/IProtectedHook.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity =0.8.7;
 
-// TODO: add natspec comments
+// TODO: remove in favor of SafeMultiOwnable
 interface IProtectedHook {
   event AllowedContractChange(address newAllowedContract);
 
