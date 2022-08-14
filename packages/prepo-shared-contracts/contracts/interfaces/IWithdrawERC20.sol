6ced113948d8bba7cd2018a30790011322d299b3
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-08-14 14:21:54 -0700

    docs: withdrawerc20 and safeownable interface copy (#306)
    * feat: withdrawerc20 and safeownable interface copy
    
    * fix: cr #306 interface nits
    
    * fix: linter

--- packages/prepo-shared-contracts/contracts/interfaces/IWithdrawERC20.sol
+++ packages/prepo-shared-contracts/contracts/interfaces/IWithdrawERC20.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity =0.8.7;
 
-// TODO: add natspec comments
+/**
+ * @notice Allows a contract owner to withdraw any ERC20 tokens residing
+ * within the contract to themselves.
+ */
 interface IWithdrawERC20 {
+  /**
+   * @notice Withdraws corresponding `amounts` of each ERC20 in `erc20Tokens`.
+   * @dev Length of `erc20Tokens` and `amounts` must match.
+   *
+   * Only callable by `owner()`.
+   * @param erc20Tokens ERC20 tokens to be withdrawn
+   * @param amounts Amounts to be withdrawn
+   */
   function withdrawERC20(address[] calldata erc20Tokens, uint256[] calldata amounts) external;
 }
