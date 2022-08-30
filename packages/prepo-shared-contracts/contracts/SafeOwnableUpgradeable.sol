ed8c3a3c86c41abffcf0d7a00c4e4261572873ce
Kumar Nilay <nilay@prepo.io>
2022-08-30 16:32:57 +0530

    fix: adding _gap in upgradeable contracts (#356)

--- packages/prepo-shared-contracts/contracts/SafeOwnableUpgradeable.sol
+++ packages/prepo-shared-contracts/contracts/SafeOwnableUpgradeable.sol
@@ -44,4 +44,11 @@ abstract contract SafeOwnableUpgradeable is ISafeOwnable, OwnableUpgradeable {
     emit NomineeUpdate(_nominee, _newNominee);
     _nominee = _newNominee;
   }
+
+  /**
+   * @dev This empty reserved space is put in place to allow future versions to add new
+   * variables without shifting down storage in the inheritance chain.
+   * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+   */
+  uint256[50] private __gap;
 }
