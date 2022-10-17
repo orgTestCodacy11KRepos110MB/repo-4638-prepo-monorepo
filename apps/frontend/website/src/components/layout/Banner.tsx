1810523039d5743f82f9fab718c9eac6ebcc6124
Chris Ling <81092286+chrisling-dev@users.noreply.github.com>
2022-10-18 07:53:27 +0800

    feat: show join public sale link on website banner (#444)

--- apps/frontend/website/src/components/layout/Banner.tsx
+++ apps/frontend/website/src/components/layout/Banner.tsx
@@ -4,15 +4,15 @@ import { Button } from '../Button'
 const Banner: React.FC = () => (
   <div className="py-[6px] px-2 text-white bg-prepo">
     <p className="text-center leading-4">
-      The PPO Token Public Sale starts October 18!{' '}
+      The PPO Token Public Sale has started!{' '}
       <Button
         className="underline cursor-pointer !p-0 font-normal text-base hover:!bg-transparent leading-4"
         iconClassName="!ml-1"
-        href={ROUTES.PUBLIC_SALE_BLOG}
+        href={ROUTES.TOKEN_SALE}
         target="_blank"
         iconSize={16}
       >
-        Learn More
+        Join now
       </Button>
     </p>
   </div>
