db93806f2596318c7ed4244a1226bf87fbf8c61b
chrisling-dev <chrisling.dev@gmail.com>
2022-11-02 14:15:43 +0800

    feat: trade as default route with query parameters

--- apps/frontend/core-dapp/src/hooks/useSelectedMarket.ts
+++ apps/frontend/core-dapp/src/hooks/useSelectedMarket.ts
@@ -2,16 +2,20 @@ import { useMemo } from 'react'
 import { useRouter } from 'next/router'
 import { useRootStore } from '../context/RootStoreProvider'
 import { MarketEntity } from '../stores/entities/MarketEntity'
+import { markets } from '../lib/markets'
 
-const useSelectedMarket = (): MarketEntity | undefined => {
+const useSelectedMarket = (): MarketEntity => {
   const { query } = useRouter()
-  const selectedMarketName = query?.marketUrlId
+  const selectedMarketName = query?.marketId
   const { marketStore } = useRootStore()
 
+  const defaultMarket = marketStore.markets[markets[0].urlId]
   return useMemo(
     () =>
-      typeof selectedMarketName === 'string' ? marketStore.markets[selectedMarketName] : undefined,
-    [marketStore.markets, selectedMarketName]
+      typeof selectedMarketName === 'string'
+        ? marketStore.markets[selectedMarketName] ?? defaultMarket
+        : defaultMarket,
+    [defaultMarket, marketStore.markets, selectedMarketName]
   )
 }
 
