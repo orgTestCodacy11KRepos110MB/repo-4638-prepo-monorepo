65632732f8be8eb34b1c749088fa70edfa82b72a
chrisling-dev <chrisling.dev@gmail.com>
2022-11-03 16:43:32 +0800

    feat: undo useSelectedMarket

--- apps/frontend/core-dapp/src/hooks/useSelectedMarket.ts
+++ apps/frontend/core-dapp/src/hooks/useSelectedMarket.ts
@@ -2,20 +2,16 @@ import { useMemo } from 'react'
 import { useRouter } from 'next/router'
 import { useRootStore } from '../context/RootStoreProvider'
 import { MarketEntity } from '../stores/entities/MarketEntity'
-import { markets } from '../lib/markets'
 
-const useSelectedMarket = (): MarketEntity => {
+const useSelectedMarket = (): MarketEntity | undefined => {
   const { query } = useRouter()
-  const selectedMarketName = query?.marketId
+  const selectedMarketName = query?.marketUrlId
   const { marketStore } = useRootStore()
 
-  const defaultMarket = marketStore.markets[markets[0].urlId]
   return useMemo(
     () =>
-      typeof selectedMarketName === 'string'
-        ? marketStore.markets[selectedMarketName] ?? defaultMarket
-        : defaultMarket,
-    [defaultMarket, marketStore.markets, selectedMarketName]
+      typeof selectedMarketName === 'string' ? marketStore.markets[selectedMarketName] : undefined,
+    [marketStore.markets, selectedMarketName]
   )
 }
 
