385cb1fb642b3e81f1ed39b390d36a52537c4bae
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-11-25 12:09:13 -0800

    feat: add IDepositTradeHelper and Uniswap packages (#545)
    * feat: add IDepositTradeHelper and Uniswap packages
    
    * fix: cr #545 updated parameters to reflect new impl and added structs
    
    * fix: cr #545 rev2 add getBaseToken, remove immutable setters
    
    * feat: cr #545 rev3 expose constant and fix amountOut parameter

---
+++ apps/smart-contracts/core/contracts/interfaces/IDepositTradeHelper.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity =0.8.7;
+
+import "./ICollateral.sol";
+import "./IPrePOMarket.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+interface IDepositTradeHelper {
+  struct Permit {
+    uint256 deadline;
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+  }
+
+  struct OffChainTradeParams {
+    address tokenOut;
+    address deadline;
+    uint256 amountOutMinimum;
+    uint160 sqrtPriceLimitX96;
+  }
+
+  /**
+   * @dev `baseTokenAmount` will be taken from msg.sender (assumes that
+   * msg.sender has already approved at least `baseTokenAmount` of the
+   * Collateral's base token) and then deposited to mint Collateral. This
+   * minted Collateral will be used entirely towards purchasing either Long or
+   * Short tokens from the corresponding UniswapV3 pool.
+   *
+   * The UniswapV3 pool address does not need to be provided since the
+   * SwapRouter will automatically identify the pool based on token inputs.
+   * @param _baseTokenAmount Base Token to be used towards position
+   * @param _baseTokenPermit Permit to let contract take user's base token
+   * @param _collateralPermit Permit to let contract take user's collateral
+   * @param _tradeParams Swap parameters determined off-chain
+   */
+  function depositAndTrade(
+    uint256 _baseTokenAmount,
+    Permit calldata _baseTokenPermit,
+    Permit calldata _collateralPermit,
+    OffChainTradeParams calldata _tradeParams
+  ) external;
+
+  function getBaseToken() external view returns (IERC20Permit);
+
+  function getCollateral() external view returns (ICollateral);
+
+  function getSwapRouter() external view returns (ISwapRouter);
+
+  function POOL_FEE_TIER() external view returns (uint256);
+}
