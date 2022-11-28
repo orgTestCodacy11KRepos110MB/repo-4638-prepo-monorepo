c8b4a49c4b606b0758b0bffca76763b8e8fbe6ae
davidprepo <david.g@prepo.io>
2022-11-28 17:12:49 +0000

    docs: docstrings for iuintvalue

--- packages/prepo-shared-contracts/contracts/interfaces/IUintValue.sol
+++ packages/prepo-shared-contracts/contracts/interfaces/IUintValue.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity =0.8.7;
 
+/**
+ * @title IUintValue
+ * @dev A generic contract that returns a uint256 value.
+ */
 interface IUintValue {
-    function get() external view returns (uint256)
+    /**
+     * @notice Returns the value.
+     * 
+     * @return the value
+     */
+    function get() external view returns (uint256);
 }
