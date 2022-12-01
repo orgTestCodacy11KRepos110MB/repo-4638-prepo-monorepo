8a2bb4c3290d08212d7640623797a47cdf058d1e
davidprepo <118175593+davidprepo@users.noreply.github.com>
2022-12-01 10:36:55 +0000

    feat: FeeRebate: create IFeeRebateHook (#596)
    * feat: interfaces
    
    * docs: documentation to interface
    
    * docs: docstrings for iuintvalue
    
    * feat: create IFeeRebateHook
    
    * fix: linter
    
    * fix: linter
    
    * fix: pr comments
    
    * fix: nit
    
    * feat: interfaces
    
    * docs: docstrings for iuintvalue
    
    * feat: create IFeeRebateHook
    
    * fix: pr comments
    
    * fix: nit

---
+++ apps/smart-contracts/core/contracts/interfaces/IFeeRebateHook.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity =0.8.7;
+
+import "./ITokenSender.sol";
+
+interface IFeeRebateHook {
+  event TreasuryChange(address treasury);
+
+  event TokenSenderChange(address sender);
+
+  function setTreasury(address treasury) external;
+
+  function setTokenSender(ITokenSender tokenSender) external;
+}
