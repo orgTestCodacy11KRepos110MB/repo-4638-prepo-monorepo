0c14fef8e06b8490b13b097169eef84a72a71bce
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-12-08 00:58:02 -0800

    docs: add collateral hook copy (#668)

--- apps/smart-contracts/core/contracts/interfaces/ICollateralHook.sol
+++ apps/smart-contracts/core/contracts/interfaces/ICollateralHook.sol
@@ -3,8 +3,10 @@ pragma solidity =0.8.7;
 
 import "./ICollateral.sol";
 
-/// @notice Used for adding additional checks and/or data recording when
-/// interacting with the Collateral vault.
+/**
+ * @notice The base contract for an external hook that adds functionality to
+ * a `Collateral` contract.
+ */
 interface ICollateralHook {
   /**
    * @dev Emitted via `setCollateral()`.
@@ -13,19 +15,19 @@ interface ICollateralHook {
   event CollateralChange(address collateral);
 
   /**
-   * @dev This hook should only contain calls to external contracts, where
-   * the actual implementation and state of a feature will reside.
+   * @dev This function should be as "stateless" as possible, and if it needs
+   * to change state it should delegate that logic to an external contract.
    *
-   * `amountBeforeFee` is the `baseToken` amount deposited/withdrawn by the
+   * `amountBeforeFee` is the Base Token amount deposited/withdrawn by the
    * caller before fees are taken.
    *
-   * `amountAfterFee` is the `baseToken` amount deposited/withdrawn by the
+   * `amountAfterFee` is the BaseToken amount deposited/withdrawn by the
    * caller after fees are taken.
    *
-   * Only callable by allowed collateral.
+   * Only callable by `collateral`.
    * @param sender Caller depositing/withdrawing collateral
-   * @param amountBeforeFee `baseToken` amount before fees
-   * @param amountAfterFee `baseToken` amount after fees
+   * @param amountBeforeFee Base Token amount before fees
+   * @param amountAfterFee Base Token amount after fees
    */
   function hook(
     address sender,
@@ -35,7 +37,7 @@ interface ICollateralHook {
 
   /**
    * @notice Sets the collateral that will be allowed to call this hook.
-   * @dev Only callable by owner().
+   * @dev Only callable by `SET_COLLATERAL_ROLE`.
    * @param newCollateral The new allowed collateral
    */
   function setCollateral(ICollateral newCollateral) external;
