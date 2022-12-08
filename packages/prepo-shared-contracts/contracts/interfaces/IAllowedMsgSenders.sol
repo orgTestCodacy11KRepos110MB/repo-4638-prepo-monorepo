49a7ed94272db013245d9364e69be713a8aef0a2
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-12-08 12:20:13 -0800

    chore: add basic caller module documentation

--- packages/prepo-shared-contracts/contracts/interfaces/IAllowedMsgSenders.sol
+++ packages/prepo-shared-contracts/contracts/interfaces/IAllowedMsgSenders.sol
@@ -4,12 +4,29 @@ pragma solidity =0.8.7;
 import "./IAccountList.sol";
 
 /**
- * @notice Inheritable module
+ * @notice Module for storing an allowlist to restrict who can call certain
+ * functions. Used throughout in our contracts for hooks or external contracts
+ * that should only be read/write to by hooks.
+ *
+ * @dev `allowedMsgSenders` is private to be explicit about the list only
+ * being usable through the `onlyAllowedMsgSenders` modifier.
  */
 interface IAllowedMsgSenders {
+  /**
+   * @dev emitted by `setAllowedMsgSenders()`
+   * @param allowedMsgSenders The new list representing allowed `msg.sender`s
+   */
   event AllowedMsgSendersChange(IAccountList allowedMsgSenders);
 
+  /**
+   * @notice Sets the list that the `onlyAllowedMsgSenders` modifier will use
+   * to restrict access.
+   * @dev This function is meant to be overriden and does not include any
+   * access controls.
+   * @param allowedMsgSenders The new list representing allowed `msg.sender`s
+   */
   function setAllowedMsgSenders(IAccountList allowedMsgSenders) external;
 
+  /// @return The list representing allowed `msg.sender`'s
   function getAllowedMsgSenders() external view returns (IAccountList);
 }
--- packages/prepo-shared-contracts/contracts/interfaces/ITokenSenderCaller.sol
+++ packages/prepo-shared-contracts/contracts/interfaces/ITokenSenderCaller.sol
@@ -3,16 +3,44 @@ pragma solidity =0.8.7;
 
 import "./ITokenSender.sol";
 
+/**
+ * @notice Module for storing a treasury address and an external
+ * `ITokenSender` contract to send tokens to users based on some criteria.
+ * @dev Typically used by a hook that sends a fee to `_treasury` and
+ * reimburses it with a token using `_tokenSender`.
+ */
 interface ITokenSenderCaller {
+  /**
+   * @dev emitted by `setTreasury()`
+   * @param treasury The new treasury address
+   */
   event TreasuryChange(address treasury);
 
-  event TokenSenderChange(address sender);
+  /**
+   * @dev emitted by `setTokenSender()`
+   * @param tokenSender The new `ITokenSender` contract
+   */
+  event TokenSenderChange(address tokenSender);
 
+  /**
+   * @notice Sets the treasury address
+   * @dev This function is meant to be overriden and does not include any
+   * access controls.
+   * @param treasury The new treasury address
+   */
   function setTreasury(address treasury) external;
 
-  function getTreasury() external view returns (address);
-
+  /**
+   * @notice Sets the token sender contract
+   * @dev This function is meant to be overriden and does not include any
+   * access controls.
+   * @param tokenSender The new external token sender contract
+   */
   function setTokenSender(ITokenSender tokenSender) external;
 
+  ///@return The treasury address
+  function getTreasury() external view returns (address);
+
+  ///@return The external token sender contract
   function getTokenSender() external returns (ITokenSender);
 }
