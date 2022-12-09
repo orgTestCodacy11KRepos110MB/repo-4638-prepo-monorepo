61109b47536d11ccf84e4190b8af7e0cd60aaa77
Chris Ling <81092286+chrisling-dev@users.noreply.github.com>
2022-12-09 21:31:18 +0800

    fix: undo website banner (#676)

--- apps/frontend/website/src/components/layout/Banner.tsx
+++ apps/frontend/website/src/components/layout/Banner.tsx
@@ -4,15 +4,15 @@ import { Button } from '../Button'
 const Banner: React.FC = () => (
   <div className="py-[6px] px-2 text-white bg-prepo">
     <p className="text-center leading-4">
-      The PPO Token Bond Program has started!{' '}
+      The PPO Token Public Sale has started!{' '}
       <Button
         className="underline cursor-pointer !p-0 font-normal text-base hover:!bg-transparent leading-4"
         iconClassName="!ml-1"
-        href={ROUTES.BOND_BLOG_POST}
+        href={ROUTES.TOKEN_SALE}
         target="_blank"
         iconSize={16}
       >
-        Learn more
+        Join now
       </Button>
     </p>
   </div>
