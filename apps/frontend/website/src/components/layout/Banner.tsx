9492c4d2641a9d395b1ca26d91716680c7640e25
Chris Ling <81092286+chrisling-dev@users.noreply.github.com>
2022-12-10 02:15:40 +0800

    feat: bond banner (#677)

--- apps/frontend/website/src/components/layout/Banner.tsx
+++ apps/frontend/website/src/components/layout/Banner.tsx
@@ -4,15 +4,15 @@ import { Button } from '../Button'
 const Banner: React.FC = () => (
   <div className="py-[6px] px-2 text-white bg-prepo">
     <p className="text-center leading-4">
-      The PPO Token Public Sale has started!{' '}
+      The PPO Token Bond Program has started!{' '}
       <Button
         className="underline cursor-pointer !p-0 font-normal text-base hover:!bg-transparent leading-4"
         iconClassName="!ml-1"
-        href={ROUTES.TOKEN_SALE}
+        href={ROUTES.BOND_BLOG_POST}
         target="_blank"
         iconSize={16}
       >
-        Join now
+        Learn more
       </Button>
     </p>
   </div>
